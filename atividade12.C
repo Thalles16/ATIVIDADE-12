a17151c4846aa0053d4524eb496da56a05205c9a
Thalles16 <168739769+Thalles16@users.noreply.github.com>
2024-06-03 10:57:27 -0300

    atividade12.C

--- atividade12.C
+++ atividade12.C
@@ -1,74 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Função para cadastrar as notas de um aluno
+void cadastrarNotas(char nome[], float notas[]) {
+    printf("Escreva o nome do aluno:\n ");
+    scanf("%s", nome);
 
-void cadastro_Alunos () {
-	
-	int i;
-	
-	char nome [5];
-	
-	int notas [20];
-	
-	float media1 = notas [0]+notas [1]notas [3]+/4;
-	
-	float media2 = notas [5+6+7+8]/4;
-	
-	float media3 = notas [9+10+11+12]/4;
-	
-	float media4 = notas [13+14+15+16]/4;
-	
-	float media5 = notas [17+18+19+20]/4;
-	
-	for (i = 0; i < 5; i++) {
-	
-	
-	printf("diga o nome dos alunos\n");
-		scanf("%s", &nome[i]);	
-	
+    printf("Escreva as notas do aluno:\n ");
+    int i;
+    for (i = 0; i < 4; ++i) {
+        scanf("%f", &notas[i]);
+    }
 }
-system("pause");
-system("cls");
-	for (i = 0; i<20; i++){
-	
-	printf("digite a notas dos alunos\n");
-		scanf("%i", &notas[i]);
-		
-		
-		
-	}
-	system("pause");
-	system("cls");
-		printf("a media dos alunos e\n %f", media1 );
-		
+
+// Função para calcular a média de um aluno e determinar seu status
+void calcularMediaEStatus(const char nome[], const float notas[]) {
+    float soma = 0;
+    int i;
+    for (i = 0; i < 4; ++i) {
+        soma += notas[i];
+    }
+    float media = soma / 4;
+
+    printf("A media do aluno %s e %.2f\n", nome, media);
+
+    if (media < 4) {
+        printf("O aluno esta reprovado\n\n");
+    } else if (media >= 6) {
+        printf("O aluno esta aprovado\n\n");
+    } else {
+        printf("O aluno esta de recuperacao\n\n");
+    }
 }
 
 int main() {
-system("color A");	
-	
-	
-	int opcao;
-	
+    const int NUM_ALUNOS = 5;
+    char nomes[NUM_ALUNOS][50];
+    float notas[NUM_ALUNOS][4];
 
-	while ((opcao != 1) && (opcao !=2) && (opcao != 3)) {		
-		// opcoes do menu
-		system("cls");
-		printf("Bem vindo ao meu sistema\n\n");
-		printf("Menu\n");
-		printf("----\n");
-		printf("1-Cadastro de Notas/Alunos\n");
-		printf("2-Alteracao de Notas/Alunos\n");
-		printf("3-Sair\n");
-		printf("Opcao: ");
-		scanf("%i",&opcao);
-		// verificando as opcoes
-		if (opcao == 1) {
-			system("cls");
-			
-			printf("CADASTRO DE ALUNOS\n\n");
-			
-			cadastro_Alunos (); 
-		}
-}
+	system("color a");
 
+    int opcao;
+    do {
+        printf("MENU\n");
+        printf("1- Cadastro das Notas dos Alunos\n");
+        printf("2- Alteracao das Notas\n");
+        printf("3- SAIR\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        system("cls");
+
+        switch (opcao) {
+            case 1:
+                system("cls");
+                printf("CADASTRO DE NOTAS DOS ALUNOS\n");
+                int i;
+				for (i = 0; i < NUM_ALUNOS; ++i) {
+                    cadastrarNotas(nomes[i], notas[i]);
+                    calcularMediaEStatus(nomes[i], notas[i]);
+                }
+                break;
+            case 2:
+                system("cls");
+                printf("ALTERACAO DE NOTAS\n");
+				for (i = 0; i < NUM_ALUNOS; ++i) {
+                    cadastrarNotas(nomes[i], notas[i]);
+                    calcularMediaEStatus(nomes[i], notas[i]);
+                }
+                break;
+            case 3:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida! Tente novamente.\n\n");
+                system("pause");
+                system("cls");
+        }
+    } while (opcao != 3);
+
+    return 0;
 }
